39bac2bb5fd36bad9f3871be852ce196203ddb5d
AbhiNav <sharmaabhinav95700@gmail.com>
2023-09-15 14:57:41 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Web  Developer", "Blogger", "ML-AI Enthusiast", "Content Writer", "Poet"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 700,
+    loop:true
+});
